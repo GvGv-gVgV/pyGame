a6b30ea1f068179bbdd8b349196b964f9fe31772
MIX PC <cyberpunkK02122008@gmail.com>
2025-01-13 19:59:42 +0400

    13.01.25

--- games.py
+++ games.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 import random
+import asyncio
 
 pygame.init()
 size = width, height = 1000, 1000
@@ -20,11 +21,31 @@ def load_image(name, colorkey=None):
     return image
 
 
+all_sprites = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+bomb_group = pygame.sprite.Group()
+SPAWN = pygame.USEREVENT + 1
+pygame.time.set_timer(SPAWN, 800)
+
+sprite = pygame.sprite.Sprite()
+sprite.image = load_image("fon.png")
+sprite.rect = sprite.image.get_rect()
+all_sprites.add(sprite)
+sprite.rect.x = -1
+
+sprite2 = pygame.sprite.Sprite()
+sprite2.image = load_image("yl2.png")
+sprite2.rect = sprite2.image.get_rect()
+all_sprites.add(sprite2)
+sprite2.rect.y = height - 200
+
+
 class Landing(pygame.sprite.Sprite):
     image = load_image("r1.png")
 
     def __init__(self):
-        super().__init__(all_sprites)
+        super().__init__(bomb_group)
+        self.add(bomb_group)
         self.image = Landing.image
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(width - 61)
@@ -36,24 +57,66 @@ class Landing(pygame.sprite.Sprite):
         # если ещё в небе
         if self.rect.y < height - 85:
             self.rect = self.rect.move(0, 2)
+        if pygame.sprite.spritecollideany(self, bullet_group):
+            self.image = load_image("boom.png")
+            self.remove(bomb_group)
 
 
-all_sprites = pygame.sprite.Group()
-sprite = pygame.sprite.Sprite()
-sprite.image = load_image("r1.png")
-sprite.rect = sprite.image.get_rect()
-all_sprites.add(sprite)
-SPAWN = pygame.USEREVENT + 1
-pygame.time.set_timer(SPAWN, 800)
+class Bullet(pygame.sprite.Sprite):
+    image = load_image("bullet (1).png")
+
+    def __init__(self, pos):
+        super().__init__(bullet_group)
+        self.add(bullet_group)
+        self.image = Bullet.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        # если ещё в небе
+        self.rect = self.rect.move(0, -10)
+        # if pygame.sprite.spritecollideany(self, bomb_group):
+        #     self.remove(bullet_group)
+
+
 running = True
+right = False
+left = False
 while running:
+    if right:
+        if sprite2.rect.x < width - 120:
+            sprite2.rect.x += 10
+    if left:
+        if sprite2.rect.x > -30:
+            sprite2.rect.x -= 10
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                right = True
+            if event.key == pygame.K_LEFT:
+                left = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                right = False
+            if event.key == pygame.K_LEFT:
+                left = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Bullet((sprite2.rect.x + 64, sprite2.rect.y + 30))
         if event.type == SPAWN:
             Landing()
     screen.fill('white')
     all_sprites.draw(screen)
     all_sprites.update()
+    bullet_group.draw(screen)
+    bullet_group.update()
+    bomb_group.draw(screen)
+    bomb_group.update()
     clock.tick(fps)
+
     pygame.display.flip()
